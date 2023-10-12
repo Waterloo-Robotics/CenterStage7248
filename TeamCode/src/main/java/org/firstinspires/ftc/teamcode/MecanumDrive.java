435dd591da30ea72398cf14e99e0e41434d24073
H2OLooBots <ftcteam7247@gmail.com>
2023-10-12 18:52:58 -0500

    12.10.2023

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.ftc.waterloo.h2oloobots.DriveTrain;
+import com.ftc.waterloo.h2oloobots.H2OLooOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Mecanum TeleOp", group = "!")
+public class MecanumDrive extends H2OLooOpMode {
+
+    @Override
+    public void opModeInit() {
+
+        driveTrain.setDriveTrainType(DriveTrain.DriveTrainType.MECANUM);
+
+    }
+
+    @Override
+    public void opModePeriodic() {
+
+        while (opModeIsActive()) {
+
+            driveTrain.teleOpDrive(
+                    gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    gamepad1.right_stick_x
+            );
+
+        }
+
+    }
+
+}
