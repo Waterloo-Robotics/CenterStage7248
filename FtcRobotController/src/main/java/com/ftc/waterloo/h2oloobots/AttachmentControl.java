debb427dec6b111a00eb8a3edf598b4564e96215
H2OLooBots <ftcteam7247@gmail.com>
2023-10-22 12:54:51 -0500

    IDK what this was

--- FtcRobotController/src/main/java/com/ftc/waterloo/h2oloobots/AttachmentControl.java
+++ FtcRobotController/src/main/java/com/ftc/waterloo/h2oloobots/AttachmentControl.java
@@ -1,7 +1,11 @@
 package com.ftc.waterloo.h2oloobots;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**This AttachmentControl class just offers a global area to store any non-drivebase commands and
  * devices to be used as chosen. Most imports should be added already, but if you need other
@@ -15,11 +19,60 @@ public class AttachmentControl {
     TelemetryControl telemetryControl;
     Gamepad gamepad1, gamepad2;
 
+    DcMotor intakeMotorLeft, intakeMotorRight;
+    MotorControlGroup intakeGroup;
+    Servo droneServo;
+
     public AttachmentControl(HardwareMap hardwareMap, TelemetryControl telemetryControl, Gamepad gamepad1, Gamepad gamepad2) {
 
         this.telemetryControl = telemetryControl;
         this.gamepad1 = gamepad1;
         this.gamepad2 = gamepad2;
+        droneServo = hardwareMap.servo.get("droneServo");
+        droneServo.scaleRange(0.35, 1);
+
+        intakeMotorLeft = hardwareMap.dcMotor.get("intakeMotorLeft");
+        intakeMotorRight = hardwareMap.dcMotor.get("intakeMotorRight");
+        intakeMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intakeGroup = new MotorControlGroup(intakeMotorLeft, intakeMotorRight);
+
+    }
+
+    public void droneManual(boolean increaseButton, boolean decreaseButton) {
+        double position = droneServo.getPosition();
+        if (increaseButton) position += 0.005;
+        else if (decreaseButton) position -= 0.05;
+
+        if (position > 1) position = 1;
+        if (position < 0) position = 0;
+        droneServo.setPosition(position);
+
+        telemetryControl.addData("Drone Servo Position", droneServo.getPosition());
+
+    }
+
+    public void droneTeleOp(boolean button) {
+
+        if (button) {
+
+            if (droneServo.getPosition() > 0.85) {
+
+                droneServo.setPosition(0);
+
+            } else {
+
+                droneServo.setPosition(1);
+
+            }
+
+        }
+
+    }
+
+    public void intakeManual(double power) {
+
+        intakeGroup.setPower(power);
 
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.ftc.waterloo.h2oloobots.H2OLooTeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class AttachmentTest extends H2OLooTeleOp {
+
+    boolean isBPressed = false;
+
+    public void opModeInit() {}
+
+    public void opModePeriodic() {
+
+        if (gamepad1.b) {
+
+            if (!isBPressed) {
+
+//                attachmentControl.droneTeleOp(true);
+
+            }
+
+            isBPressed = true;
+
+        } else {
+
+            isBPressed = false;
+
+        }
+
+        attachmentControl.intakeManual(gamepad2.left_stick_x);
+
+        driveTrain.teleOpDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
+    }
+
+}
