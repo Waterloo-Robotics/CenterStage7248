5aef401a75d3b08a792013324ddadfd4f905d498
H2OLooBots <ftcteam7247@gmail.com>
2023-11-07 18:55:46 -0600

    adding PenguinTele

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PenguinTele.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.ftc.waterloo.h2oloobots.DriveTrain;
+import com.ftc.waterloo.h2oloobots.H2OLooTeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+
+@TeleOp(name = "Madagas Tele", group = "!")
+public class PenguinTele extends H2OLooTeleOp {
+
+    Servo servo1;
+
+    boolean isAPressed = false;
+
+    @Override
+    public void opModeInit() {
+
+        driveTrain.setDriveTrainType(DriveTrain.DriveTrainType.MECANUM);
+
+    }
+
+    @Override
+    public void opModePeriodic() {
+
+        driveTrain.teleOpDrive(
+                gamepad1.left_stick_y * 0.7,
+                gamepad1.left_stick_x * 0.7,
+                gamepad1.right_stick_x * 0.7
+        );
+
+        if (gamepad1.a) {
+
+            if (!isAPressed) {
+
+                attachmentControl.droneTeleOp(true);
+
+            }
+
+            isAPressed = true;
+
+        } else {
+
+            isAPressed = false;
+
+        }
+
+        attachmentControl.intakeTeleOp();
+
+    }
+
+}
